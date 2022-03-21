2a49fb8bed69036130d6a27bffd395c2847702b0
Gary Larson <gary.larson316@gmail.com>
2022-03-20 18:31:08 -0700

    all detail views functioning

--- finder/urls.py
+++ finder/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('get_areas/', views.get_areas, name='areas'),
     path('get_all_pathways/', views.get_all_pathways, name='all_pathways'),
     path('get_all_degrees/', views.get_all_degrees, name='all_degrees'),
+
+    # filtered paths
+    path('get_pathways/<int:id>', views.get_pathways, name='pathways'),
+
+    # detailed paths  --NOTE-- name given in url is the one that must be used with url keyword in .html file
+    path('get_pathway_detail/<int:path_id>', views.get_pathway_detail, name='path_detail'),
+    path('get_area_detail/<int:area_id>', views.get_area_detail, name='area_detail'),
+    path('get_degree_detail/<int:degree_id>', views.get_degree_detail, name='degree_detail'),
 ]
--- finder/views.py
+++ finder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import AreaOfStudy, Pathway, Degree
 
 
@@ -21,3 +21,36 @@ def get_all_degrees(request):
     degrees = Degree.objects.order_by("revised_date").all()
     return render(request, 'finder/all_degrees.html', {'degrees': degrees})
 
+
+# filtered views
+def get_pathways(request, area_id):
+    """
+    use a for loop to iterate through objects in .html file
+    :param request: request object handled by django
+    :param area_id: id of area-of-study
+    :return: list of Pathways under that area of study
+    """
+    filtered_paths = Pathway.objects.filter(area=area_id)
+    # todo: play with this 'filterPaths' string to see if it is the key to ref context
+    return render(request, 'finder/all_pathways.html', {'filteredPaths': filtered_paths})
+
+
+# detail views
+def get_pathway_detail(request, path_id):
+    path = get_object_or_404(Pathway, pk=path_id)
+    degrees = Degree.objects.filter(pathway_id=path.pk)
+    context = {
+        'path': path,
+        'degrees': degrees
+    }
+    return render(request, 'finder/pathway_detail.html', {'context': context})
+
+
+def get_area_detail(request, area_id):
+    area = get_object_or_404(AreaOfStudy, pk=area_id)
+    return render(request, 'finder/area_of_study_detail.html', {'area': area})
+
+
+def get_degree_detail(request, degree_id):
+    degree = get_object_or_404(Degree, pk=degree_id)
+    return render(request, 'finder/degree_detail.html', {'degree': degree})
